5c49df6427fd37b36d948373285df97cffdedb6b
Rodolfo Martin <rodolfomartin@eprosima.com>
2009-09-30 10:02:51 +0000

    copyright, notice

--- idl2ddscs/src/com/eprosima/ddscs/idl/DumpVisitor.java
+++ idl2ddscs/src/com/eprosima/ddscs/idl/DumpVisitor.java
@@ -1,5 +1,17 @@
-/**
- * 
+/* $Id$   *
+ * (c) Copyright, eProsima, 2009.                                          *
+ * All rights reserved.                                                    *    
+ *                                                                         *
+ * No duplications, whole or partial, manual or electronic, may be made    *
+ * without express written permission.  Any such copies, or                *
+ * revisions thereof, must display this notice unaltered.                  *
+ * This code contains trade secrets of                                     *
+ * eProsima (Proyectos y Sistemas de Mantenimiento S.L.)                   *
+ *                                                                         *
+ * modification history                                                    *
+ * --------------------                                                    *
+ * 1.0,29sep09,RodM Created                                                *
+ * =====================================================================   *
  */
 
 /**
