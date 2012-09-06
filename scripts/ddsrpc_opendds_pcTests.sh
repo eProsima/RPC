d21514a1f0c6679dee2052f2dc34f252241cd582
Ricardo Gonzalez <ricardogonzalez@eprosima.com>
2012-09-06 08:31:38 +0000

    - Change in Linux scripts

--- scripts/ddsrpc_opendds_pcTests.sh
+++ scripts/ddsrpc_opendds_pcTests.sh
@@ -5,6 +5,4 @@ if [ x$JAVA_HOME = x ]; then
 	exit 1
 fi
 
-
-
 exec "${JAVA_HOME}/bin/java" -DPATH="$PATH" -DLD_LIBRARY_PATH="$LD_LIBRARY_PATH" -DTAO_ROOT="$TAO_ROOT" -DDDS_ROOT="$DDS_ROOT" -Djava.ext.dirs="../../../../idl2ddsrpc/lib" com.eprosima.ddsrpc.idl.IDL2DDSRPC -middleware opendds -os Linux "$@"
