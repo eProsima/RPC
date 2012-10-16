d2a794dc87f48bea596d69e1f26c9de46e9eb65b
Ricardo Gonzalez <ricardogonzalez@eprosima.com>
2012-10-16 15:31:16 +0000

    - Updated documentation.

--- include/utils/Messages.h
+++ include/utils/Messages.h
@@ -5,15 +5,23 @@ namespace eProsima
 {
 	namespace DDSRPC
 	{
-
+		/**
+		 * \brief This enumeration type defines the success of an operation or
+		 *        the posible errors that occur in the operation.
+		 */
 		typedef enum ReturnMessage
 		{
+			/// \brief An error occurs in the server's proxy side.
 			CLIENT_ERROR = -2,
-			RECEIVED_OTHER_REQUEST,
+			/// \brief Timeout while the client is waiting the server's reply.
 			SERVER_TIMEOUT,
+			/// \brief An error occurs in the server side.
 			SERVER_ERROR,
+			/// \brief There are not resources to manage the server's proxy request.
 			WITHOUT_RESOURCES,
+			/// \brief Operation was succesful.
 			OPERATION_SUCCESSFUL,
+			/// \brief Server was not found.
 			NO_SERVER
 		} ReturnMessage;
 
