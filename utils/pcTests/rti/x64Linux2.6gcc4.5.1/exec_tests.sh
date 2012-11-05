0c0eba83bd452e9d1b7bc20c5e9e5e73862e6807
Ricardo Gonzalez <ricardogonzalez@eprosima.com>
2012-11-05 12:38:01 +0000

    New script for tests in x64 Linux

---
+++ utils/pcTests/rti/x64Linux2.6gcc4.5.1/exec_tests.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+errorstatus=0
+
+function execTest
+{
+# Backup of Client.cxx file.
+cp Client.cxx Client.cxx.backup
+# Backup of the ServerImpl.cxx file
+cp $1ServerImpl.cxx $1ServerImpl.cxx.backup
+# Delete all generated files.
+rm *.h *.cxx $1RequestReply.idl
+# Generates the file with DDSRPC script
+../../../../../scripts/ddsrpc_rti_pcTests.sh -ppDisable "$1.idl"
+errorstatus=$?
+if [ $errorstatus != 0 ]; then return; fi
+# Copy backup to original files.
+mv Client.cxx.backup Client.cxx
+mv $1ServerImpl.cxx.backup $1ServerImpl.cxx
+# Clean output directory
+rm -rf objs
+# Compile client application
+make -f makefile_x64Linux2.6gcc4.5.1
+errorstatus=$?
+if [ $errorstatus != 0 ]; then return; fi
+# Compile server application
+make -f makefile_x64Linux2.6gcc4.5.1 server
+errorstatus=$?
+if [ $errorstatus != 0 ]; then return; fi
+# Execute the server in background
+objs/x64Linux2.6gcc4.5.1/$1Server &
+# Wait 5 seconds
+sleep 5
+# Execute the client
+objs/x64Linux2.6gcc4.5.1/$1Client
+errorstatus=$?
+if [ $errorstatus != 0 ]; then return; fi
+# Kill server
+killall -9 $1Server
+}
+
+# This script runs all tests in this directory and checks their results.
+for dir in $(find . -type d -mindepth 1 -maxdepth 1 -printf "%f\n"); do
+# Enter to the directory.
+cd $dir
+execTest $dir
+# Go out to the parent directory.
+cd ..
+# Detect error in call.
+if [ $errorstatus != 0 ]; then
+break
+fi
+done
+
+if [ $errorstatus == 0 ]; then
+echo "TEST SUCCESSFULLY"
+else
+echo "TEST FAILED"
+fi
+
+exit $errorstatus
