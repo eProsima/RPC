891538139ad1a38d3c2342e990a76d7359cb38e0
Ruben Rubio <rubenrubio@eprosima.com>
2013-12-03 14:36:32 +0000

    AnnotationTypeCode added.

---
+++ rpcddsgen/src/com/eprosima/rpcdds/typecode/AnnotationTypeCode.java
@@ -0,0 +1,71 @@
+package com.eprosima.rpcdds.typecode;
+
+import com.eprosima.rpcdds.util.Pair;
+
+import java.util.List;
+
+import org.antlr.stringtemplate.StringTemplate;
+
+public class AnnotationTypeCode extends MemberedTypeCode
+{
+    public AnnotationTypeCode(String scope, String name)
+    {
+        super(TypeCode.KIND_ANNOTATION, scope, name);
+    }
+    
+    @Override
+    public String getTypename()
+    {
+        StringTemplate st = getTypenameFromStringTemplate();
+        st.setAttribute("name", getScopedname());
+        return st.toString();
+    }
+    
+    @Override
+    public String getStTypename()
+    {
+        StringTemplate st = getSTTypenameFromStringTemplate();
+        st.setAttribute("name", getScopedname());
+        return st.toString();
+    }
+    
+    public Pair<Integer, Integer> getMaxSerializedSize(int currentSize, int lastDataAligned)
+    {
+        List<Member> members = getMembers();
+        int lcurrentSize = currentSize;
+        int llastDataAligned = lastDataAligned;
+        
+        for(int count = 0; count < members.size(); ++count)
+        {
+            Pair<Integer, Integer> pair = members.get(count).getTypecode().getMaxSerializedSize(lcurrentSize, llastDataAligned);
+            lcurrentSize = pair.first();
+            llastDataAligned = pair.second();
+        }
+        
+        return new Pair<Integer, Integer>(lcurrentSize, llastDataAligned);
+    }
+    
+    public int getMaxSerializedSizeWithoutAlignment(int currentSize)
+    {
+        List<Member> members = getMembers();
+        int lcurrentSize = currentSize;
+        
+        for(int count = 0; count < members.size(); ++count)
+        {
+            lcurrentSize = members.get(count).getTypecode().getMaxSerializedSizeWithoutAlignment(lcurrentSize);
+        }
+        
+        return lcurrentSize;
+    }
+    
+    public String getMaxSerializedSize()
+    {
+        Pair<Integer, Integer> pair = getMaxSerializedSize(0, 0);
+        return pair.first().toString();
+    }
+    
+    public String getMaxSerializedSizeWithoutAlignment()
+    {
+        return Integer.toString(getMaxSerializedSizeWithoutAlignment(0));
+    }
+}
