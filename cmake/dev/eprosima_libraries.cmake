7ecffa5454a9b157dde5df56a16e431583a98f55
Ricardo González Moreno <RicardoGonzalez@eprosima.com>
2016-09-09 08:17:00 +0200

    Fixed cmake script

--- cmake/dev/eprosima_libraries.cmake
+++ cmake/dev/eprosima_libraries.cmake
@@ -22,10 +22,10 @@ macro(find_eprosima_package package)
                 "\${SOURCE_DIR_}"
                 "\${GENERATOR_}"
                 ${BUILD_OPTION}
-		"-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
-		${USE_BOOST}
+                "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+                ${USE_BOOST_}
                 "-DMINION=ON"
-		"-DEPROSIMA_INSTALLER_MINION=${EPROSIMA_INSTALLER_MINION}"
+                "-DEPROSIMA_INSTALLER_MINION=${EPROSIMA_INSTALLER_MINION}"
                 "-DBIN_INSTALL_DIR:PATH=${BIN_INSTALL_DIR}"
                 "-DINCLUDE_INSTALL_DIR:PATH=${INCLUDE_INSTALL_DIR}"
                 "-DLIB_INSTALL_DIR:PATH=${LIB_INSTALL_DIR}"
@@ -64,12 +64,12 @@ macro(find_eprosima_package package)
             endif()
 
             if(MSVC OR MSVC_IDE)
-		if("${CMAKE_BUILD_TYPE}" MATCHES "^([Dd][Ed][Bb][Uu][Gg])$")
-		    set(BUILD_TYPE_GENERATION "Release")
-		else()
-		    set(BUILD_TYPE_GENERATION ${CMAKE_BUILD_TYPE})
-		endif()
-		
+                if("${CMAKE_BUILD_TYPE}" MATCHES "^([Dd][Ed][Bb][Uu][Gg])$")
+                    set(BUILD_TYPE_GENERATION "Release")
+                else()
+                    set(BUILD_TYPE_GENERATION ${CMAKE_BUILD_TYPE})
+                endif()
+
                 execute_process(COMMAND ${CMAKE_COMMAND} --build . --config Debug
                     WORKING_DIRECTORY ${${package}ExternalDir}
                     RESULT_VARIABLE EXECUTE_RESULT
@@ -79,13 +79,13 @@ macro(find_eprosima_package package)
                     message(FATAL_ERROR "Cannot build Git submodule ${package} in debug mode")
                 endif()
 
-		execute_process(COMMAND ${CMAKE_COMMAND} --build . --config ${BUILD_TYPE_GENERATION} 
+                execute_process(COMMAND ${CMAKE_COMMAND} --build . --config ${BUILD_TYPE_GENERATION} 
                     WORKING_DIRECTORY ${${package}ExternalDir}
                     RESULT_VARIABLE EXECUTE_RESULT
                     )
 
                 if(NOT EXECUTE_RESULT EQUAL 0)
-			message(FATAL_ERROR "Cannot build Git submodule ${package} in ${BUILD_TYPE_GENERATION} mode")
+                    message(FATAL_ERROR "Cannot build Git submodule ${package} in ${BUILD_TYPE_GENERATION} mode")
                 endif()
             else()
                 execute_process(COMMAND ${CMAKE_COMMAND} --build .
@@ -101,7 +101,7 @@ macro(find_eprosima_package package)
             set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ${CMAKE_INSTALL_PREFIX_})
         endif()
 
-	find_package(${package} QUIET)
+        find_package(${package} QUIET)
 
         if(${package}_FOUND)
             message(STATUS "${package} library found...")
@@ -129,11 +129,11 @@ macro(install_eprosima_libraries)
                 OPTIONAL
                 )
         else()
-	    if("${CMAKE_BUILD_TYPE}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
-		set(BUILD_TYPE_INSTALLATION "Release")
-	    else()
-		set(BUILD_TYPE_INSTALLATION ${CMAKE_BUILD_TYPE})
-	    endif()
+            if("${CMAKE_BUILD_TYPE}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+                set(BUILD_TYPE_INSTALLATION "Release")
+            else()
+                set(BUILD_TYPE_INSTALLATION ${CMAKE_BUILD_TYPE})
+            endif()
 
             # Install includes
             install(DIRECTORY ${PROJECT_BINARY_DIR}/external/install/${INCLUDE_INSTALL_DIR}/
@@ -193,26 +193,26 @@ macro(install_eprosima_libraries)
                 )
         endif()
     elseif(UNIX AND EPROSIMA_BUILD AND NOT MINION AND NOT EPROSIMA_INSTALLER)
-            # Install includes
-            install(DIRECTORY ${PROJECT_BINARY_DIR}/external/install/${INCLUDE_INSTALL_DIR}/
-                DESTINATION ${INCLUDE_INSTALL_DIR}
-                COMPONENT headers
-                OPTIONAL
-                )
+        # Install includes
+        install(DIRECTORY ${PROJECT_BINARY_DIR}/external/install/${INCLUDE_INSTALL_DIR}/
+            DESTINATION ${INCLUDE_INSTALL_DIR}
+            COMPONENT headers
+            OPTIONAL
+            )
 
-            # Install libraries
-            install(DIRECTORY ${PROJECT_BINARY_DIR}/external/install/${LIB_INSTALL_DIR}/
-                DESTINATION ${LIB_INSTALL_DIR}
-                USE_SOURCE_PERMISSIONS
-                COMPONENT libraries
-                )
+        # Install libraries
+        install(DIRECTORY ${PROJECT_BINARY_DIR}/external/install/${LIB_INSTALL_DIR}/
+            DESTINATION ${LIB_INSTALL_DIR}
+            USE_SOURCE_PERMISSIONS
+            COMPONENT libraries
+            )
 
-            # Install licenses
-            install(DIRECTORY ${PROJECT_BINARY_DIR}/external/install/licenses/
-                DESTINATION ${LICENSE_INSTALL_DIR}
-                COMPONENT licenses
-                OPTIONAL
-                )
+        # Install licenses
+        install(DIRECTORY ${PROJECT_BINARY_DIR}/external/install/licenses/
+            DESTINATION ${LICENSE_INSTALL_DIR}
+            COMPONENT licenses
+            OPTIONAL
+            )
     endif()
 endmacro()
 
