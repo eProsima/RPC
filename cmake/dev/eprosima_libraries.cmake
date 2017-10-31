b948cab5c06ce915990e0680645b5535be122ca8
Ricardo González Moreno <correoricky@gmail.com>
2017-10-31 14:47:29 +0100

    Refs #2315. Support for multiple same thirdparty

--- cmake/dev/eprosima_libraries.cmake
+++ cmake/dev/eprosima_libraries.cmake
@@ -12,98 +12,107 @@ macro(find_eprosima_package package)
                 endif()
             endforeach()
 
-            set(${package}ExternalDir ${PROJECT_BINARY_DIR}/external/${package})
+            set(EPROSIMA_PACKAGE_EXTERNAL_DIR "" CACHE STRING "External directory to compile eprosima libraries")
 
-            if(MINION)
-                set(CMAKE_INSTALL_PREFIX_ "${CMAKE_INSTALL_PREFIX}")
+            if("${EPROSIMA_PACKAGE_EXTERNAL_DIR}" STREQUAL "")
+                set(${package}ExternalDir ${PROJECT_BINARY_DIR}/external/${package})
+                set(EPROSIMA_PACKAGE_EXTERNAL_DIR ${PROJECT_BINARY_DIR}/external)
             else()
-                set(CMAKE_INSTALL_PREFIX_ "${PROJECT_BINARY_DIR}/external/install")
+                set(${package}ExternalDir ${EPROSIMA_PACKAGE_EXTERNAL_DIR}/${package})
             endif()
 
-            # Separate CMAKE_PREFIX_PATH
-            string(REPLACE ";" "|" CMAKE_PREFIX_PATH_ "${CMAKE_PREFIX_PATH}")
-            set(${package}_CMAKE_ARGS
-                "\${SOURCE_DIR_}"
-                "\${GENERATOR_}"
-                ${BUILD_OPTION}
-                "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
-                "-DCMAKE_CXX_COMPILER_LAUNCHER=${CMAKE_CXX_COMPILER_LAUNCHER}"
-                ${LIST_OF_OPTIONS}
-                "-DMINION=ON"
-                "-DEPROSIMA_INSTALLER_MINION=${EPROSIMA_INSTALLER_MINION}"
-                "-DBIN_INSTALL_DIR:PATH=${BIN_INSTALL_DIR}"
-                "-DINCLUDE_INSTALL_DIR:PATH=${INCLUDE_INSTALL_DIR}"
-                "-DLIB_INSTALL_DIR:PATH=${LIB_INSTALL_DIR}"
-                "-DLICENSE_INSTALL_DIR:PATH=licenses"
-                "\${CMAKE_INSTALL_PREFIX_}"
-                "\${CMAKE_PREFIX_PATH_}"
-                )
-            list(APPEND ${package}_CMAKE_ARGS LIST_SEPARATOR "|")
-
-            file(MAKE_DIRECTORY ${${package}ExternalDir})
-            file(WRITE ${${package}ExternalDir}/CMakeLists.txt
-                "cmake_minimum_required(VERSION 2.8.11)\n"
-                "include(ExternalProject)\n"
-                "set(SOURCE_DIR_ \"${PROJECT_SOURCE_DIR}/thirdparty/${package}\")\n"
-                "set(GENERATOR_ -G \"${CMAKE_GENERATOR}\")\n"
-                "set(CMAKE_INSTALL_PREFIX_ \"-DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX_}\")\n"
-                "set(CMAKE_PREFIX_PATH_ -DCMAKE_PREFIX_PATH=\"${CMAKE_PREFIX_PATH_}\")\n"
-                "ExternalProject_Add(${package}\n"
-                "CONFIGURE_COMMAND \"${CMAKE_COMMAND}\"\n"
-                "${${package}_CMAKE_ARGS}\n"
-                "DOWNLOAD_COMMAND echo\n"
-                "UPDATE_COMMAND cd \"${PROJECT_SOURCE_DIR}\" && git submodule update --recursive --init \"thirdparty/${package}\"\n"
-                "SOURCE_DIR \${SOURCE_DIR_}\n"
-                "BINARY_DIR \"${${package}ExternalDir}/build\"\n"
-                ")\n")
-
-            if(NOT "$ENV{CMAKE_MAKEFLAGS}" STREQUAL "")
-                set(ENV{MAKEFLAGS} "$ENV{CMAKE_MAKEFLAGS}")
-            endif()
-
-            execute_process(COMMAND ${CMAKE_COMMAND}
-                -G ${CMAKE_GENERATOR}
-                ${BUILD_OPTION}
-                WORKING_DIRECTORY ${${package}ExternalDir}
-                RESULT_VARIABLE EXECUTE_RESULT
-                )
-
-            if(NOT EXECUTE_RESULT EQUAL 0)
-                message(FATAL_ERROR "Cannot configure Git submodule ${package}")
-            endif()
-
-            if(MSVC OR MSVC_IDE)
-                if("${CMAKE_BUILD_TYPE}" MATCHES "^([Dd][Ed][Bb][Uu][Gg])$")
-                    set(BUILD_TYPE_GENERATION "Release")
+            if(NOT EXISTS "${${package}ExternalDir}/cmake_install.cmake")
+                if(MINION)
+                    set(CMAKE_INSTALL_PREFIX_ "${CMAKE_INSTALL_PREFIX}")
                 else()
-                    set(BUILD_TYPE_GENERATION ${CMAKE_BUILD_TYPE})
+                    set(CMAKE_INSTALL_PREFIX_ "${PROJECT_BINARY_DIR}/external/install")
                 endif()
 
-                execute_process(COMMAND ${CMAKE_COMMAND} --build . --config Debug
+                # Separate CMAKE_PREFIX_PATH
+                string(REPLACE ";" "|" CMAKE_PREFIX_PATH_ "${CMAKE_PREFIX_PATH}")
+                set(${package}_CMAKE_ARGS
+                    "\${SOURCE_DIR_}"
+                    "\${GENERATOR_}"
+                    "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+                    "-DCMAKE_CXX_COMPILER_LAUNCHER=${CMAKE_CXX_COMPILER_LAUNCHER}"
+                    ${LIST_OF_OPTIONS}
+                    "-DMINION=ON"
+                    "-DEPROSIMA_INSTALLER_MINION=${EPROSIMA_INSTALLER_MINION}"
+                    "-DBIN_INSTALL_DIR:PATH=${BIN_INSTALL_DIR}"
+                    "-DINCLUDE_INSTALL_DIR:PATH=${INCLUDE_INSTALL_DIR}"
+                    "-DLIB_INSTALL_DIR:PATH=${LIB_INSTALL_DIR}"
+                    "-DLICENSE_INSTALL_DIR:PATH=licenses"
+                    "\${CMAKE_INSTALL_PREFIX_}"
+                    "\${CMAKE_PREFIX_PATH_}"
+                    "\${EPROSIMA_PACKAGE_EXTERNAL_DIR_}"
+                    )
+                list(APPEND ${package}_CMAKE_ARGS LIST_SEPARATOR "|")
+
+                file(MAKE_DIRECTORY ${${package}ExternalDir})
+                file(WRITE ${${package}ExternalDir}/CMakeLists.txt
+                    "cmake_minimum_required(VERSION 2.8.11)\n"
+                    "include(ExternalProject)\n"
+                    "set(SOURCE_DIR_ \"${PROJECT_SOURCE_DIR}/thirdparty/${package}\")\n"
+                    "set(GENERATOR_ -G \"${CMAKE_GENERATOR}\")\n"
+                    "set(CMAKE_INSTALL_PREFIX_ \"-DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX_}\")\n"
+                    "set(CMAKE_PREFIX_PATH_ -DCMAKE_PREFIX_PATH=\"${CMAKE_PREFIX_PATH_}\")\n"
+                    "set(EPROSIMA_PACKAGE_EXTERNAL_DIR_ -DEPROSIMA_PACKAGE_EXTERNAL_DIR=\"${EPROSIMA_PACKAGE_EXTERNAL_DIR}\")\n"
+                    "ExternalProject_Add(${package}\n"
+                    "CONFIGURE_COMMAND \"${CMAKE_COMMAND}\"\n"
+                    "${${package}_CMAKE_ARGS}\n"
+                    "DOWNLOAD_COMMAND echo\n"
+                    "UPDATE_COMMAND cd \"${PROJECT_SOURCE_DIR}\" && git submodule update --recursive --init \"thirdparty/${package}\"\n"
+                    "SOURCE_DIR \${SOURCE_DIR_}\n"
+                    "BINARY_DIR \"${${package}ExternalDir}/build\"\n"
+                    ")\n")
+
+                if(NOT "$ENV{CMAKE_MAKEFLAGS}" STREQUAL "")
+                    set(ENV{MAKEFLAGS} "$ENV{CMAKE_MAKEFLAGS}")
+                endif()
+
+                execute_process(COMMAND ${CMAKE_COMMAND}
+                    -G ${CMAKE_GENERATOR}
                     WORKING_DIRECTORY ${${package}ExternalDir}
                     RESULT_VARIABLE EXECUTE_RESULT
                     )
 
                 if(NOT EXECUTE_RESULT EQUAL 0)
-                    message(FATAL_ERROR "Cannot build Git submodule ${package} in debug mode")
+                    message(FATAL_ERROR "Cannot configure Git submodule ${package}")
                 endif()
 
-                execute_process(COMMAND ${CMAKE_COMMAND} --build . --config ${BUILD_TYPE_GENERATION} 
-                    WORKING_DIRECTORY ${${package}ExternalDir}
-                    RESULT_VARIABLE EXECUTE_RESULT
-                    )
+                if(MSVC OR MSVC_IDE)
+                    if("${CMAKE_BUILD_TYPE}" MATCHES "^([Dd][Ed][Bb][Uu][Gg])$")
+                        set(BUILD_TYPE_GENERATION "Release")
+                    else()
+                        set(BUILD_TYPE_GENERATION ${CMAKE_BUILD_TYPE})
+                    endif()
 
-                if(NOT EXECUTE_RESULT EQUAL 0)
-                    message(FATAL_ERROR "Cannot build Git submodule ${package} in ${BUILD_TYPE_GENERATION} mode")
-                endif()
-            else()
-                execute_process(COMMAND ${CMAKE_COMMAND} --build .
-                    WORKING_DIRECTORY ${${package}ExternalDir}
-                    RESULT_VARIABLE EXECUTE_RESULT
-                    )
+                    execute_process(COMMAND ${CMAKE_COMMAND} --build . --config Debug
+                        WORKING_DIRECTORY ${${package}ExternalDir}
+                        RESULT_VARIABLE EXECUTE_RESULT
+                        )
 
-                if(NOT EXECUTE_RESULT EQUAL 0)
-                    message(FATAL_ERROR "Cannot build Git submodule ${package}")
+                    if(NOT EXECUTE_RESULT EQUAL 0)
+                        message(FATAL_ERROR "Cannot build Git submodule ${package} in debug mode")
+                    endif()
+
+                    execute_process(COMMAND ${CMAKE_COMMAND} --build . --config ${BUILD_TYPE_GENERATION} 
+                        WORKING_DIRECTORY ${${package}ExternalDir}
+                        RESULT_VARIABLE EXECUTE_RESULT
+                        )
+
+                    if(NOT EXECUTE_RESULT EQUAL 0)
+                        message(FATAL_ERROR "Cannot build Git submodule ${package} in ${BUILD_TYPE_GENERATION} mode")
+                    endif()
+                else()
+                    execute_process(COMMAND ${CMAKE_COMMAND} --build .
+                        WORKING_DIRECTORY ${${package}ExternalDir}
+                        RESULT_VARIABLE EXECUTE_RESULT
+                        )
+
+                    if(NOT EXECUTE_RESULT EQUAL 0)
+                        message(FATAL_ERROR "Cannot build Git submodule ${package}")
+                    endif()
                 endif()
             endif()
 
@@ -237,3 +246,5 @@ macro(check_threadpool)
         message(FATAL_ERROR "Failed updating Git submodule boost-threadpool")
     endif()
 endmacro()
+
+
