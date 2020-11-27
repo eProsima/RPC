df4fc80aa29a63f95fb291715ad94ea821c5a61f
Ricardo González <correoricky@gmail.com>
2020-11-27 10:21:47 +0100

    Colcon support (#13)
    * Add colcon.pkg
    
    * Using dependencies through colcon
    
    * Remove old code
    
    * Always use fastdds master branch
    
    * Refs #9862. Fix compilation on Linux
    
    * Apply suggestions from code review
    
    Co-authored-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Update include/transports/TCPServerTransport.h
    
    Co-authored-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Fork to eprosima
    
    Co-authored-by: Miguel Company <miguelcompany@eprosima.com>

--- src/strategies/ThreadPoolStrategy.cpp
+++ src/strategies/ThreadPoolStrategy.cpp
@@ -1,10 +1,10 @@
 /*************************************************************************
- * Copyright (c) 2013 eProsima. All rights reserved.
- *
- * This copy of FASTRPC is licensed to you under the terms described in the
- * FASTRPC_LICENSE file included in this distribution.
- *
- *************************************************************************/
+* Copyright (c) 2013 eProsima. All rights reserved.
+*
+* This copy of FASTRPC is licensed to you under the terms described in the
+* FASTRPC_LICENSE file included in this distribution.
+*
+*************************************************************************/
 
 #include <strategies/ThreadPoolStrategy.h>
 #include "ServerStrategyImpl.h"
@@ -15,79 +15,86 @@
 
 static const char* const CLASS_NAME = "ThreadPoolStrategy";
 
-namespace eprosima
+namespace eprosima {
+namespace rpc {
+namespace strategy {
+
+class ThreadPoolStrategyJob
 {
-    namespace rpc
+public:
+
+    ThreadPoolStrategyJob(
+            boost::function<void()> callback)
+        : m_callback(callback)
     {
-        namespace strategy
-        {
-            class ThreadPoolStrategyJob
-            {
-                public:
-                    ThreadPoolStrategyJob(boost::function<void()> callback)
-                        : m_callback(callback)
-                    {
-                    }
+    }
 
-                    void run()
-                    {
-                        m_callback();
-                    }
+    void run()
+    {
+        m_callback();
+    }
 
-                private:
+private:
 
-                    boost::function<void()> m_callback;
-            };
+    boost::function<void()> m_callback;
+};
 
-            class ThreadPoolStrategyImpl : public ServerStrategyImpl
-            {
-                public:
+class ThreadPoolStrategyImpl : public ServerStrategyImpl
+{
+public:
 
-                    ThreadPoolStrategyImpl(unsigned int threadCount)
-                    {
-                        m_pool = new boost::threadpool::pool(threadCount);
-                    }
+    ThreadPoolStrategyImpl(
+            unsigned int threadCount)
+    {
+        m_pool = new boost::threadpool::pool(threadCount);
+    }
 
-                    virtual ~ThreadPoolStrategyImpl()
-                    {
-                        delete m_pool;
-                    }
+    virtual ~ThreadPoolStrategyImpl()
+    {
+        delete m_pool;
+    }
 
-                    boost::threadpool::pool* getPool()
-                    {
-                        return m_pool;
-                    }
-					
-					void schedule(boost::function<void()> callback)
-					{
-						boost::shared_ptr<ThreadPoolStrategyJob> job(new ThreadPoolStrategyJob(callback));
-						boost::threadpool::schedule(*m_pool, boost::bind(&ThreadPoolStrategyJob::run, job));
-					}
+    boost::threadpool::pool* getPool()
+    {
+        return m_pool;
+    }
 
-                private:
+    void schedule(
+            boost::function<void()> callback)
+    {
+        boost::shared_ptr<ThreadPoolStrategyJob> job(new ThreadPoolStrategyJob(callback));
+        m_pool->schedule(boost::threadpool::task_functor<void>(boost::bind(&ThreadPoolStrategyJob::run, job)));
+    }
 
-                    boost::threadpool::pool *m_pool;
-            };
-        } // namespace strategy
-    } // namespace rpc
+private:
+
+    boost::threadpool::pool* m_pool;
+};
+
+} // namespace strategy
+} // namespace rpc
 } // namespace eprosima
 
 using namespace eprosima::rpc;
 using namespace ::strategy;
 using namespace ::transport;
 
-ThreadPoolStrategy::ThreadPoolStrategy(unsigned int threadCount) : m_impl(NULL)
+ThreadPoolStrategy::ThreadPoolStrategy(
+        unsigned int threadCount)
+    : m_impl(NULL)
 {
     m_impl = new ThreadPoolStrategyImpl(threadCount);
 }
 
 ThreadPoolStrategy::~ThreadPoolStrategy()
 {
-    if(m_impl != NULL)
+    if (m_impl != NULL)
+    {
         delete m_impl;
+    }
 }
 
 ServerStrategyImpl* ThreadPoolStrategy::getImpl()
 {
-	return m_impl;
+    return m_impl;
 }
